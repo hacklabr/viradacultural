9e3ce0d9ef7a5b64b2dc0ed0b6889684f87fd082
Leo Germani <leogermani@hacklab.com.br>
2014-04-04 18:09:25 -0300

    arquivos de configuracao do grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "src/wp-content/themes/viradacultural/main.css": "src/wp-content/themes/viradacultural/less/main.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        // Which files to watch (all .less files recursively in the less directory)
+        files: ['src/wp-content/themes/viradacultural/less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+  grunt.registerTask('default', ['watch']);
+};
