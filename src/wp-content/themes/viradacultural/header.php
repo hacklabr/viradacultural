fd4acca63de87d82daf9fa93b877bf7786f34051
Ariadne <ariadne@hacklab.com.br>
2014-04-07 19:13:10 -0300

    fix #17 template de página, single e arquivos

--- src/wp-content/themes/viradacultural/header.php
+++ src/wp-content/themes/viradacultural/header.php
@@ -35,20 +35,32 @@
         ?></title>
 
         <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />        
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/elegant-font.css" />
         <!-- Bootstrap -->
-        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ) ?>/main.css" />        
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ) ?>/main.css" />
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->        
-        
-        <?php wp_head(); ?>        
+        <![endif]-->
+
+        <?php wp_head(); ?>
     </head>
     <body <?php if (get_query_var('virada_tpl')): ?> class="programacao" <?php else: ?><?php body_class(); ?><?php endif; ?>>
+
+        <!-- Facebook code -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=248328698638549";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        <!-- Facebook code -->
+
         <header id="main-header" <?php if (get_query_var('virada_tpl')): ?>class="col-md-1"<?php else: ?>class="col-md-2"<?php endif; ?>>
             <div id="brand">
                 <h1 id="logo-virada" class="logo"><a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>"><span class="sr-only"><?php bloginfo( 'name' ); ?></span></a></h1>
@@ -71,8 +83,8 @@
                         </ul>
                     </li>
                     <li><a class="anos-10" href="<?php bloginfo( 'url' ); ?>/10-anos" title="10 anos"><span>10 anos</span></a></li>
-                    <li><a class="programacao" href="<?php bloginfo( 'url' ); ?>/programacao" title="Programação"><span>Programação</span></a></li>                    
-                    <li><a class="noticias" href="<?php echo esc_url( $noticias_link ); ?>" title="Notícias"><span>Notícias</span></a></li>
+                    <li><a class="programacao" href="<?php bloginfo( 'url' ); ?>/programacao" title="Programação"><span>Programação</span></a></li>
+                    <li><a class="noticias" href="<?php echo get_post_type_archive_link( 'noticias' ); ?>" title="Notícias"><span>Notícias</span></a></li>
                     <li><a class="blog" href="<?php echo esc_url( $blog_link ); ?>" title="Blog"><span>Blog</span></a></li>
                     <li><a class="imprensa" href="<?php echo get_post_type_archive_link( 'imprensa' ); ?>" title="Imprensa"><span>Imprensa</span></a></li>
                     <li><a class="nas-redes" href="<?php bloginfo( 'url' ); ?>/nas-redes" title="Nas redes"><span>Nas redes</span></a></li>
@@ -85,3 +97,51 @@
             <h2 id="logo-smc" class="logo"><a href="http://www.prefeitura.sp.gov.br/"><span class="sr-only">Secretaria Municipal de Cultural de São Paulo</span></a></h2>
         </header>
         <!-- #main-header -->
+
+        <?php if (is_page() || is_single()) { ?>
+        <nav id="programacao-navbar" class="navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="col-md-8 col-md-offset-2">
+                    <?php if ('noticias' == get_post_type()) { ?>
+                        <h1>Notícias</h1>
+                    <?php } else if ('imprensa' == get_post_type()) { ?>
+                        <h1>Imprensa</h1>
+                    <?php } else if (is_single()) { ?>
+                        <h1>Blog</h1>
+                    <?php } ?>
+                    <div class="share-buttons alignright">
+                        <div class="facebook">
+                            <div class="fb-share-button" data-href="#" data-type="button_count"></div>
+                        </div>
+                        <div class="twitter">
+                            <a href="https://twitter.com/share" class="twitter-share-button" data-via="virada" data-lang="pt">Tweetar</a>
+                            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+                        </div>
+                        <div class="g-plus">
+                            <!-- Place this tag where you want the share button to render. -->
+                            <div class="g-plus" data-action="share" data-annotation="bubble"></div>
+                            <!-- Place this tag after the last share tag. -->
+                            <script type="text/javascript">
+                              window.___gcfg = {lang: 'pt-BR'};
+
+                              (function() {
+                                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+                                po.src = 'https://apis.google.com/js/platform.js';
+                                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+                              })();
+                            </script>
+                        </div>
+                    </div><!-- .share-buttons -->
+                </div>
+            </div>
+        </nav>
+        <?php } ?> <!-- if is_page || is_single -->
+        <?php if (is_search()) { ?>
+        <nav id="programacao-navbar" class="navbar navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>Resultados de busca para: <?php echo get_search_query(); ?></h1>
+                </div>
+            </div>
+        </nav>
+        <?php } ?> <!-- if is_page || is_single -->
--- src/wp-content/themes/viradacultural/page.php
+++ src/wp-content/themes/viradacultural/page.php
@@ -2,28 +2,28 @@
 <div class="container-fluid">
 	<div class="row">
 		<section id="main-section" class="col-md-8 col-md-offset-2">
-			<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>			
-				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix');?>>	  
-					<header>                       
-						<h1><<?php the_title();?></h1>
-						<p><?php edit_post_link( __( 'Edit', 'viradacultural' ), '', '' ); ?></p>				
+			<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix');?>>
+					<header>
+						<h1><?php the_title();?></h1>
+						<p><?php edit_post_link( __( 'Edit', 'viradacultural' ), '', '' ); ?></p>
 					</header>
 					<div class="post-content clearfix">
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<nav class="page-link">' . __( 'Pages:', 'viradacultural' ), 'after' => '</nav>' ) ); ?>	
+						<?php wp_link_pages( array( 'before' => '<nav class="page-link">' . __( 'Pages:', 'viradacultural' ), 'after' => '</nav>' ) ); ?>
 					</div>
 					<!-- .post-content -->
 				</article>
 				<!-- .page -->
-			<?php endwhile; ?>				
+			<?php endwhile; ?>
 			<?php else : ?>
-			   <p><?php _e('No results found.', 'viradacultural'); ?></p>              
+			   <p><?php _e('No results found.', 'viradacultural'); ?></p>
 			<?php endif; ?>
 		</section>
 		<!-- #main-section -->
 		<?php get_footer(); ?>
 	</div>
-	<!-- .row -->         
+	<!-- .row -->
 </div>
 <!-- .container-fluid -->
 <?php html::part('countdown'); ?>
