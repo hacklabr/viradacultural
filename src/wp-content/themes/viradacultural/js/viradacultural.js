53456d7839f4f24c3475496b5c14df45aecea3e8
Anderson Orui <contato@andersonorui.com.br>
2014-04-08 08:02:25 -0300

    changing tab for spaces

--- src/wp-content/themes/viradacultural/js/viradacultural.js
+++ src/wp-content/themes/viradacultural/js/viradacultural.js
@@ -1,15 +1,15 @@
 (function($){
-	$(document).ready(function(){
+    $(document).ready(function(){
 
-		$('.btn-sm span').tooltip();
+        $('.btn-sm span').tooltip();
 
-		$('.collapse').collapse();
+        $('.collapse').collapse();
 
         $('.timepicker-field').timepicker();
 
-		$('#front-page-carousel .item:first, .carousel-indicators li:first').addClass('active');
+        $('#front-page-carousel .item:first, .carousel-indicators li:first').addClass('active');
 
-		$('#front-page-carousel').carousel();
+        $('#front-page-carousel').carousel();
         
         // MENU ANIMATION
         $("#main-header li.has-children").hover(
@@ -37,5 +37,5 @@
                 $("#grid-view").removeClass("active");
             }
         )
-	})
+    })
 })(jQuery);
