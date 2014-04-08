8b1e60a8b281ff42a5958c22924b452a972047f9
Cátia <catia@hacklab.com.br>
2014-04-08 11:24:16 -0300

    crazyness

--- src/wp-content/themes/viradacultural/page-dez-anos.php
+++ src/wp-content/themes/viradacultural/page-dez-anos.php
@@ -6,30 +6,100 @@ Template Name: 10 anos
 
 <?php get_header(); ?>
 <div class="container-fluid">
-	<div class="row">
-		<section id="main-section" class="col-md-8 col-md-offset-2">
-			<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>			
-				<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix');?>>	  
-					<header>                       
-						<h1><?php the_title();?></h1>
-						<p><?php edit_post_link( __( 'Edit', 'viradacultural' ), '', '' ); ?></p>				
-					</header>
-					<div class="post-content clearfix">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<nav class="page-link">' . __( 'Pages:', 'viradacultural' ), 'after' => '</nav>' ) ); ?>	
-					</div>
-					<!-- .post-content -->
-				</article>
-				<!-- .page -->
-			<?php endwhile; ?>				
-			<?php else : ?>
-			   <p><?php _e('No results found.', 'viradacultural'); ?></p>              
-			<?php endif; ?>
-		</section>
-		<!-- #main-section -->
-		<?php get_footer(); ?>
-	</div>
-	<!-- .row -->         
+    <section id="main-section" class="col-md-offset-2 virada-10-anos">
+        <?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class('row parent');?>>
+                <header>
+                    <h1><?php the_title();?></h1>
+                </header>
+                <figure>
+                    <?php if ( has_post_thumbnail() ) the_post_thumbnail("full", array("class" => "background-image")); ?>
+                </figure>
+                <section class="block col-md-8 col-md-offset-2">
+                    <div class="centered">
+                        <?php the_content(); ?>
+                        <p><?php edit_post_link( __( 'Edit', 'viradacultural' ), '', '' ); ?></p>
+                    </div>
+                </section>
+            </article>
+
+            <?php
+                $children = new WP_Query( array( 'post_parent' => $post->ID, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC', 'nopaging' => true));
+                if( $children->have_posts() ) : while( $children->have_posts() ) : $children->the_post();
+            ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class('row children');?>>
+                    <header>
+                        <h1><?php the_title(); ?></h1>
+                    </header>
+                    <figure>
+                        <?php the_post_thumbnail("full", array("class" => "background-image")); ?>
+                    </figure>
+                    <section class="post-content clearfix">
+                        <?php the_content(); ?>
+                    </section>
+                    <button class="btn btn-large btn-success">Baixar programação</button>
+                </article>
+                
+            <?php endwhile; endif; ?>
+            <!-- .page -->
+        <?php endwhile; ?>
+        <?php else : ?>
+           <p><?php _e('No results found.', 'viradacultural'); ?></p>
+        <?php endif; ?>
+    </section>
+    <!-- #main-section -->
+    <?php get_footer(); ?>
 </div>
 <!-- .container-fluid -->
-<?php html::part('countdown'); ?>
+<?php // html::part('countdown'); ?>
+
+
+<script type="text/javascript" charset="utf-8">
+(function($){
+    $(document).ready(function() {
+        var theWindow        = $(window),
+            $bg              = $("figure > img"),
+            aspectRatio      = $bg.width() / $bg.height();
+
+        $("#main-section > article").height(theWindow.height() - 55);
+        $("#main-section > article.parent > .block").height(theWindow.height() - 100);
+
+        function resizeBg() {
+            if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+                $bg
+                    .removeClass()
+                    .addClass('bgheight');
+            } else {
+                $bg
+                    .removeClass()
+                    .addClass('bgwidth');
+            }
+        }
+        theWindow.resize(resizeBg).trigger("resize");
+
+        $("article > figure > img").each(function(index) {
+            $(this).attr("id", "parallax-image-"+index);
+            index++;
+        })
+
+        for (var i = 0; i < $("article").size(); i++) {
+            $("#main-section").append("<div id='trigger-"+i+"' class='trigger'></div>");
+        }
+
+        var controller = new ScrollMagic();
+        
+        var scenes = [];
+        var tweens = [];
+        for (var i = 0; i < $("article").size(); i++) {
+            if (i == 0) {
+                tweens[i] = TweenMax.fromTo("#parallax-image-"+i, 0.5, { "opacity": 1 }, { "opacity": 1 } )
+                scenes[i] = new ScrollScene({triggerElement: "#trigger-"+i, duration: 0, offset: -(theWindow.height() - 50) }).setTween(tweens[i]).addTo(controller);
+            } else {
+                tweens[i] = TweenMax.fromTo("#parallax-image-"+i, 0.5, { "opacity": 0, "visibility": "hidden" }, { "opacity": 1, "visibility": "visible" } )
+                scenes[i] = new ScrollScene({triggerElement: "#trigger-"+i, duration: 0, offset: (theWindow.height() - 55) * i }).setTween(tweens[i]).addTo(controller);
+            }
+            // scenes[i].addIndicators();
+        }
+    })
+})(jQuery);
+</script>
