cdaf220667eb51ea5d24128e5de952bdad71b22b
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-04-10 17:24:06 -0300

    link para os espaços

--- src/wp-content/themes/viradacultural/app/services.js
+++ src/wp-content/themes/viradacultural/app/services.js
@@ -7,7 +7,7 @@
         function DataService($http) {
             var DataService = {};
 
-            var spacesDefer = $http.get(conf.templateURL+'/app/spaces.json');
+            var spacesDefer = $http.get(conf.templateURL + '/app/spaces.json');
             DataService.getSpaces = function() {
                 return spacesDefer;
             };
