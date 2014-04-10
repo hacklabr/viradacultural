ba2da8631087b5489ce4892b605618dee4433bc9
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2014-04-10 13:49:41 -0300

    corrige comportamento do botão de fechar modal

--- src/wp-content/themes/viradacultural/programacao.php
+++ src/wp-content/themes/viradacultural/programacao.php
@@ -6,7 +6,8 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="icon icon_close"></span></button>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"
+                        ng-click="$parent.filters.spaces = $parent.filters.spaces && countSelected() > 0"><span class="icon icon_close"></span></button>
                 <h4 class="modal-title" id="myModalLabel">Mapa da Virada</h4>
             </div>
             <div class="modal-body clearfix">
