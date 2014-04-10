6c97dc948f5797522356d11c5ac6ecddf3565988
Ariadne <ariadne@hacklab.com.br>
2014-04-09 21:43:52 -0300

    arruma paginação e entre posts e páginas de arquivo

--- src/wp-content/themes/viradacultural/index.php
+++ src/wp-content/themes/viradacultural/index.php
@@ -7,19 +7,23 @@
                 <?php endwhile; ?>
                 <?php if ($wp_query->max_num_pages > 1) : ?>
                     <nav id="posts-nav" class="clearfix">
-                        <div class="alignleft"><?php next_posts_link(__('&laquo; Previous posts', 'viradacultural')); ?></div>
-                        <div class="alignright"><?php previous_posts_link(__('Next posts &raquo;', 'viradacultural')); ?></div>
+                        <div class="alignleft">
+                            <?php next_posts_link('<span class="icon arrow_triangle-left"></span><span class="label">Posts anteriores</span>'); ?>
+                        </div>
+                        <div class="alignright">
+                            <?php previous_posts_link('<span class="icon arrow_triangle-right"></span><span class="label">Próximos posts</span>'); ?>
+                        </div>
                     </nav>
                     <!-- #posts-nav -->
-                <?php endif; ?>					
+                <?php endif; ?>
             <?php else : ?>
-                <p><?php _e('No results found.', 'viradacultural'); ?></p>              
+                <p><?php _e('No results found.', 'viradacultural'); ?></p>
             <?php endif; ?>
         </section>
         <!-- #main-section -->
-        <?php get_footer(); ?>	          
+        <?php get_footer(); ?>
     </div>
-    <!-- .row -->         
+    <!-- .row -->
 </div>
 <!-- .container-fluid -->
 <?php html::part('countdown'); ?>
