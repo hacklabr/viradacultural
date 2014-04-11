42f362067d3f4500ffc3611247b72418807eb8a9
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-04-11 20:16:48 -0300

    Grunt default task now compiles less before watching

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,5 +28,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
  
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['less', 'watch']);
 };
