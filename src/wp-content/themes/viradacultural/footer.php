7790de6511a040d2af2ae935db25a00bffdf64ad
Cátia <catia@hacklab.com.br>
2014-04-12 17:34:24 -0300

    html refactor

--- src/wp-content/themes/viradacultural/footer.php
+++ src/wp-content/themes/viradacultural/footer.php
@@ -1,7 +1,12 @@
-	<footer id="main-footer" class="col-md-8  col-md-offset-2">
-		<div class="clearfix>	
-			<p class="creditos textright alignright"><?php _e('Developed by', 'viradacultural'); ?> <a href="http://hacklab.com.br" title="Hacklab"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/hacklab.png" alt="" /></a> <?php _e('with', 'viradacultural'); ?> <a href="http://wordpress.org" title="WordPress"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/wp.png" alt="" /></a></p>
-		</div>
+	<footer id="main-footer" class="row">	
+		<p class="creditos textright alignright">
+			<a href="http://hacklab.com.br" title="Hacklab">
+				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/hacklab.png" alt="" />
+			</a>
+			<a href="http://wordpress.org" title="WordPress">
+				<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/wp.png" alt="" />
+			</a>
+		</p>
 	</footer>
 	<!-- #main-footer -->
 <?php wp_footer(); ?>
