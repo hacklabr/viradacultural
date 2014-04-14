03c226bd6434fbae70b2401227908e0fbcc14cb4
Leo Germani <leogermani@hacklab.com.br>
2014-04-13 23:08:21 -0300

    minha virada: atualizando visualizacao das estrelas

--- src/wp-content/themes/viradacultural/includes/minha-virada.php
+++ src/wp-content/themes/viradacultural/includes/minha-virada.php
@@ -25,6 +25,8 @@ class MinhaVirada {
 				status     : false,
 				xfbml      : true
 				});
+				
+				minhaVirada.init();
 			};
 
 			(function(d, s, id){
--- src/wp-content/themes/viradacultural/js/minha-virada.js
+++ src/wp-content/themes/viradacultural/js/minha-virada.js
@@ -9,6 +9,18 @@ minhaVirada = {
     picture: false,
     events: false,
     
+    init: function() {
+        // ao carregar a pagina vemos se o usuario ja esta conectado e com o app autorizado.
+        // se nao estiver, não fazemos nada. Só vamos fazer alguma coisa se ele clicar
+        
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                minhaVirada.initializeUserData(response, false);
+            } 
+        });
+        
+    },
+    
     connect: function(callback) {
         var callback = 'minhaVirada.' + callback + '()';
         
@@ -62,7 +74,9 @@ minhaVirada = {
                 // não sei pq se usar o metodo getJSON ao inves de post, não funciona
                 //console.log(data);
                 minhaVirada.events = data;
-                eval(callback);             
+                minhaVirada.atualizaEstrelas();
+                if (callback)
+                    eval(callback);             
             });
             
             
@@ -87,8 +101,15 @@ minhaVirada = {
         var userJSON = minhaVirada.prepareJSON();
         jQuery.post( vars.ajaxurl, {action: 'minhavirada_updateJSON', dados: userJSON }, function( data ) {
             // atualiza estrelas
-            
+            minhaVirada.atualizaEstrelas();
         });
+    },
+    
+    atualizaEstrelas: function() {
+        jQuery('.favorite').removeClass('active');
+        for (var i = 0; i < minhaVirada.events.length; i++) {
+            jQuery('.favorite-event-'+minhaVirada.events[i]).addClass('active');
+        }
     },
     
     // retorna falso se não tem, ou o índice se tem
@@ -110,7 +131,7 @@ minhaVirada = {
         //console.log(minhaVirada.eventId);
         if (minhaVirada.eventId) {
             var has_event = minhaVirada.has_event(minhaVirada.eventId);
-            if (has_event) {
+            if (has_event !== false ) { // o indice pode ser 0
                 minhaVirada.events.splice(has_event, 1);
             } else {
                 minhaVirada.events.push(minhaVirada.eventId);
