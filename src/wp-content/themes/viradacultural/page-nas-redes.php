876a9cf0d0d7ce2921549453148b412cffe30283
Ariadne <ariadnevmelo@gmail.com>
2014-04-16 19:12:08 -0300

    arruma botão de carregar mais da página nas redes

--- src/wp-content/themes/viradacultural/page-nas-redes.php
+++ src/wp-content/themes/viradacultural/page-nas-redes.php
@@ -12,7 +12,7 @@ add_action('wp_print_scripts', function () {
 
 <?php get_header(); ?>
 
-<?php 
+<?php
 //depois do header
 global $paged;
 query_posts(array(
@@ -25,22 +25,20 @@ query_posts(array(
 
 ?>
 
-<div class="container-fluid container-menu-large">        
+<div class="container-fluid container-menu-large">
 	<section id="main-section" class="row">
-        
+
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <?php html::part('loop-redes'); ?>
         <?php endwhile; ?>
-            				
+
         <?php else : ?>
-            <p>Nada encontrado. Publique no twitter ou Instagram com a hashtag #<?php echo get_theme_option('hashtag'); ?></p>              
+            <p>Nada encontrado. Publique no twitter ou Instagram com a hashtag #<?php echo get_theme_option('hashtag'); ?></p>
         <?php endif; ?>
 	</section>
-    <div class="col-md-8 col-md-offset-2">
-        <button type="button" class="btn btn-default clear" id="load-more">Carregar mais</button>
-    </div>
+    <button type="button" class="btn btn-default btn-show-more clear col-md-12" id="load-more">Carregar mais</button>
 	<!-- #main-section -->
-	<?php get_footer(); ?>      
+	<?php get_footer(); ?>
 </div>
 <!-- .container-fluid -->
 <?php html::part('countdown'); ?>
