6aadb49fb2e07ef45140eb0207d63c925d31850b
Anderson Orui <contato@andersonorui.com.br>
2014-04-17 11:32:48 -0300

    arrumando single do evento pro responsivo

--- src/wp-content/themes/viradacultural/programacao-atracoes-single.php
+++ src/wp-content/themes/viradacultural/programacao-atracoes-single.php
@@ -5,7 +5,7 @@ $atracao = get_query_var('virada_object');
 <div class="container-fluid container-menu-large" ng-controller='evento'>
     <section id="main-section" class="row">
         <img ng-src="{{conf.baseURL}}/wp-content/uploads/2014/04/Virada-Cultural-2013_pequeno_cidadão-foto_sylvia_masini-17.jpg" alt="{{event.name}}"/>
-        <article id="event-00" class="event-single">
+        <article id="event-00" class="event-single col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
             <header>
                 <h1>{{event.name}}</h1>
             </header>
@@ -26,8 +26,7 @@ $atracao = get_query_var('virada_object');
                 <a target="_blank" href="http://maps.google.com/maps?q=teatro municipal de sao paulo, Praça Ramos de Azevedo, s/n, São Paulo&hl=pt-BR&ll=-23.5451833,-46.6397523&z=17z">
                     <button class="btn btn-primary btn-xs"><span class="icon icon_pin"></span> Ver no mapa</button>
                 </a>
-                <br><br>
-                <iframe width="100%" height="220" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=pt-BR&amp;geocode=&amp;q=Teatro Municipal, Praça Ramos de Azevedo, s/n - Republica São Paulo - SP 01037-010, Brasil&amp;sll=-23.545235,-46.638615&amp;ie=UTF8&amp;hq=Teatro Municipal, Praça Ramos de Azevedo, s/n - Republica São Paulo - SP 01037-010, Brasil&amp;hnear=&amp;radius=15000&amp;t=m&amp;ll=-23.545235,-46.638615&amp;z=17&amp;output=embed&amp;iwloc=near&amp;language=pt-BR&amp;region=br"></iframe>
+                <iframe class="hidden-sm hidden-xs" width="100%" height="220" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=pt-BR&amp;geocode=&amp;q=Teatro Municipal, Praça Ramos de Azevedo, s/n - Republica São Paulo - SP 01037-010, Brasil&amp;sll=-23.545235,-46.638615&amp;ie=UTF8&amp;hq=Teatro Municipal, Praça Ramos de Azevedo, s/n - Republica São Paulo - SP 01037-010, Brasil&amp;hnear=&amp;radius=15000&amp;t=m&amp;ll=-23.545235,-46.638615&amp;z=17&amp;output=embed&amp;iwloc=near&amp;language=pt-BR&amp;region=br"></iframe>
             </div>
         </article>
         <!-- .event-single -->
