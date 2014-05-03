c936ac569c7667c12e8ebab161e06eca9a75d229
Leonardo Santagada <santagada@gmail.com>
2014-05-03 14:15:20 -0300

    removendo colisões que impedem o deploy

--- src/wp-content/themes/viradacultural/includes/extra-db-config-sample.php
+++ src/wp-content/themes/viradacultural/includes/extra-db-config-sample.php
@@ -19,7 +19,7 @@ $db_config = array(
 /* Criar essas tabelas:
 
 No banco minha_virada:
- 
+
 CREATE TABLE IF NOT EXISTS users (
 user_id INT NOT NULL PRIMARY KEY,
 data VARCHAR(2048)
@@ -27,5 +27,15 @@ data VARCHAR(2048)
 
 No banco nas_redes:
 
-
-
+CREATE TABLE `items` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `ref_id` varchar(100) NOT NULL,
+    `type` varchar(20) NOT NULL,
+    `content` text NOT NULL,
+    `date` datetime NOT NULL,
+    `author_username` varchar(255) NOT NULL,
+    `author_fullname` varchar(255) NOT NULL,
+    `text` varchar(255) DEFAULT NULL,
+    `link` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
