fb897513ab1e35f7e2b42abc57076a93f96149eb
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-05-08 15:43:47 -0300

    Cadastrando feeds de nas redes com 3 horas a menos. DB update para os já cadastrados.

--- src/wp-content/themes/viradacultural/includes/db-updates.php
+++ src/wp-content/themes/viradacultural/includes/db-updates.php
@@ -1,7 +1,18 @@
 <?php
-/*
-if (congelado_db_update('db-update-1')) {
+
+if (congelado_db_update('db-update-nas-redes-time')) {
     // execute algum código aqui
+
+    include (__DIR__.'/Simple-Database-PHP-Class/Db.php');
+    include (__DIR__.'/extra-db-config.php');
+    $db = new Db('mysql',
+        $db_config['virada_nas_redes']['host'],
+        $db_config['virada_nas_redes']['name'],
+        $db_config['virada_nas_redes']['user'],
+        $db_config['virada_nas_redes']['pass']
+    );
+
+	$db->query( "UPDATE items SET date = SUBTIME(date, '03:00:00')", array());
 }
- */
+
 ?>
