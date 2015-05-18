5dbe076c4c4f53d2a4536fa63e2277c60f896a0e
Cátia Kitahara <catia@hacklab.com.br>
2015-05-18 16:02:15 -0300

    removi as classes do botão de carregar mais

--- src/wp-content/themes/viradacultural-2015/js/nasredes.js
+++ src/wp-content/themes/viradacultural-2015/js/nasredes.js
@@ -3,7 +3,7 @@
 
         var fila = new Array();
         var limiteNaPagina = 500;
-        
+
         var buscaNovidades = function () {
 			$.ajax({
 				type: 'POST',
@@ -13,39 +13,39 @@
 					action: 'get_nasredes_posts',
                     what: 'newer'
 				},
-				
+
 				complete: function(request, status) {
 					var results = request.responseText;
 					if (results != '') {
 						$('#main-section').prepend(results);
-                        window.adustGridHeight();					
+                        window.adustGridHeight();
 					}
-					
+
 				}
 			});
 		}
-		
-		
+
+
 		var imprimeNovidades = function (){
-			
+
             if ($('#main-section > article:visible').eq(0).prev().size() > 0) {
                 $('#main-section > article:visible').eq(0).prev().fadeIn();
-                
+
                 // Limpando o fim da página
                 if ($('#main-section > article:visible').size() > limiteNaPagina)
                     $('#main-section > article:last').remove();
             }
-        
-            
-        
+
+
+
             setTimeout(imprimeNovidades, Math.floor((Math.random()*5)*1000));
-        
+
 		}
-        
+
         $('#load-more').click(function() {
             var $botao = $(this);
-            $botao.html('Carregando').removeClass('btn-default').addClass('btn-info');
-            
+            $botao.html('Carregando');
+
             $.ajax({
 				type: 'POST',
 				url: GlobalConfiguration.templateURL + '/includes/nas-redes-ajax.php',
@@ -54,26 +54,26 @@
 					action: 'get_nasredes_posts',
                     what: 'older'
 				},
-				
+
 				complete: function(request, status) {
 					var results = request.responseText;
 					if (results != '') {
 						$('#main-section').append(results);
-						$botao.html('Carregar mais').removeClass('btn-info').addClass('btn-default');
+						//$botao.html('Carregar mais').removeClass('btn-info').addClass('btn-default');
                         window.adustGridHeight();
 					} else {
-                        $botao.html('Não há mais novidades').removeClass('btn-info').addClass('btn-warning');
+                        $botao.html('Não há mais novidades');
                     }
-					
+
 				}
 			});
         });
-        
-		
+
+
 		//init
 		var buscandoNovidades = setInterval(buscaNovidades, 5000);
 		setTimeout(imprimeNovidades, 7000);
-        
+
         //$('#panic').click(function(){ clearInterval(buscandoNovidades) });
 
     })
