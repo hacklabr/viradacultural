c5ab1c6230b6d8d1368a23721cf8c6776553e3f1
Cátia Kitahara <catia@hacklab.com.br>
2015-05-20 15:13:30 -0300

    esconde os patrocinadores

--- src/wp-content/themes/viradacultural-2015/footer.php
+++ src/wp-content/themes/viradacultural-2015/footer.php
@@ -27,10 +27,10 @@
                 </ul>
             </li>
         </ul>
-        <p class="text-center">
+        <!--<p class="text-center">
             <?php html::image("logos-990.png", "", array("class" => "img-responsive visible-sm visible-md visible-lg")); ?>
             <?php html::image("logos-320.png", "", array("class" => "img-responsive visible-xs")); ?>
-        </p>
+        </p>-->
         <p class="text-center">Desenvolvido por <a id="hacklab" href="http://www.hacklab.com.br" title="hacklab/" target="_blank"><?php html::image("hacklab.png", "", array("class" => "hacklab")); ?></a> utilizando a API de dados do SP CUltura - plataforma desenvolvida em uma parceria entre o <a href="http://institutotim.org.br" title="Instituto TIM" target="_blank"><?php html::image("instituto-tim-white.png", "", array("class" => "instituto-tim")); ?></a> e a Secretaria Municipal de Cultura</p>
         <p class="text-center">Contato: <a href="mailto:smcimprensa@prefeitura.sp.gov.br">smcimprensa@prefeitura.sp.gov.br</a></p>
     </footer>
