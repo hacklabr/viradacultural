cc6cdb1a2932f8b86d668c6101b885e14a489b58
Bruno Martin <bruno.martin@hacklab.com.br>
2015-06-03 18:31:28 -0300

    corrigido rodapé

--- src/wp-content/themes/viradacultural-2015/footer.php
+++ src/wp-content/themes/viradacultural-2015/footer.php
@@ -31,7 +31,7 @@
             <?php html::image("logos-990.png", "", array("class" => "img-responsive visible-sm visible-md visible-lg")); ?>
             <?php html::image("logos-320.png", "", array("class" => "img-responsive visible-xs")); ?>
         </p>-->
-        <p class="text-center">Desenvolvido por <a id="hacklab" href="http://www.hacklab.com.br" title="hacklab/" target="_blank"><?php html::image("hacklab.png", "", array("class" => "hacklab")); ?></a> a partir da API do SP CUltura, intalação do software livre Mapas Culturais, criado em uma parceria entre o <a href="http://institutotim.org.br" title="Instituto TIM" target="_blank"><?php html::image("instituto-tim-white.png", "", array("class" => "instituto-tim")); ?></a> e a Secretaria Municipal de Cultura</p>
+        <p class="text-center">Desenvolvido por <a id="hacklab" href="http://www.hacklab.com.br" title="hacklab/" target="_blank"><?php html::image("hacklab.png", "", array("class" => "hacklab")); ?></a> a partir da API do SP CUltura, instalação do software livre Mapas Culturais, criado em uma parceria entre o <a href="http://institutotim.org.br" title="Instituto TIM" target="_blank"><?php html::image("instituto-tim-white.png", "", array("class" => "instituto-tim")); ?></a> e a Secretaria Municipal de Cultura</p>
         <p class="text-center">Contato: <a href="mailto:smcimprensa@prefeitura.sp.gov.br">smcimprensa@prefeitura.sp.gov.br</a></p>
     </footer>
     <!-- #main-footer -->
