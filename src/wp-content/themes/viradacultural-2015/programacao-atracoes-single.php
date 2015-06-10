463a6d4a6da6355a2171d51b6cb9d59d01b0a908
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-06-09 21:24:39 -0300

    ajustes na single da programação

--- src/wp-content/themes/viradacultural-2015/programacao-atracoes-single.php
+++ src/wp-content/themes/viradacultural-2015/programacao-atracoes-single.php
@@ -23,6 +23,7 @@
                     <span ng-if='event.terms.linguagem.length > 0'><span>Linguagem:</span> {{event.terms.linguagem.join(', ')}} <br></span>
                     <span ng-if='event.classificacaoEtaria'><span>Classificação:</span> {{event.classificacaoEtaria}}<br></span>
                     <span ng-if='event.acessibilidade.length > 0'><span>Acessibilidade:</span> {{event.acessibilidade.join(', ')}}<br></span>
+                    <span ng-if="event.price"><span>Ingresso:</span> {{event.price}}<br></span>
                 </p>
                 <div class="hidden">URL : {{mapUrl}}</div>
 
